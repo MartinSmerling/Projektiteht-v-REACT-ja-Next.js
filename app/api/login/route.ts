c356faf08a4d5300c8ae84e011132f24c4edd7cc
Martin Smerling <martin.smerling@gmail.com>
2026-05-13 18:00:08 +0300

    Add login logic

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -1,5 +1,16 @@
 import { prisma } from '@/lib/prisma';
+import { NextResponse } from 'next/server';
 
 export async function POST(req: Request) {
   const data = await req.json();
+  
+  const user = await prisma.user.findUnique({
+    where: { email: data.email }
+  });
+
+  if (!user || user.password !== data.password) {
+    return NextResponse.json({ error: 'Virheellinen tunnus' }, { status: 401 });
+  }
+
+  return NextResponse.json({ ok: true });
 }
